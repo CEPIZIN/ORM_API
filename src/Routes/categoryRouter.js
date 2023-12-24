c2b98108919f42c16cee094139766d7d05cec0a5
CEPIZIN~ <lauramathias03@gmail.com>
2023-12-24 02:56:13 -0300

    update routes

--- src/Routes/categoryRouter.js
+++ src/Routes/categoryRouter.js
@@ -8,6 +8,8 @@ const router = Router();
 
 router
     .get('/category', (req, res) => categoryServiceController.getAll(req, res))
-    .put('/category/:id', (req, res) => categoryServiceController.update(req, res));
-
+    .get('/category/:id',(req, res)=> categoryServiceController.GetDataByiD(req,res))
+    .put('/category/:id', (req, res) => categoryServiceController.update(req, res))    
+    .post('/category', (req,res)=> categoryServiceController.create(req,res))
+    .delete('/people/:id',(req,res)=> categoryServiceController.deleteData(req,res))
 module.exports = router;
--- src/Routes/courseRouter.js
+++ src/Routes/courseRouter.js
@@ -8,5 +8,8 @@ const router = Router();
 router
     .get('/course', (req, res) => courseController.getAll(req, res))
     .put('/course/:id', (req,res)=> courseController.update(req,res)) 
+    .put('/course/:id', (req, res) => categoryServiceController.update(req, res))    
+    .post('/course', (req,res)=> categoryServiceController.create(req,res))
+    .delete('/course/:id',(req,res)=> categoryServiceController.deleteData(req,res))
 
 module.exports = router;

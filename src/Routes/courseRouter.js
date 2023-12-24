9a686bb741b2b70212982809c004c4df9e5a6a40
CEPIZIN~ <lauramathias03@gmail.com>
2023-12-24 02:56:51 -0300

    Update courseRouter.js

--- src/Routes/courseRouter.js
+++ src/Routes/courseRouter.js
@@ -8,8 +8,8 @@ const router = Router();
 router
     .get('/course', (req, res) => courseController.getAll(req, res))
     .put('/course/:id', (req,res)=> courseController.update(req,res)) 
-    .put('/course/:id', (req, res) => categoryServiceController.update(req, res))    
-    .post('/course', (req,res)=> categoryServiceController.create(req,res))
-    .delete('/course/:id',(req,res)=> categoryServiceController.deleteData(req,res))
+    .put('/course/:id', (req, res) => courseController.update(req, res))    
+    .post('/course', (req,res)=> courseController.create(req,res))
+    .delete('/course/:id',(req,res)=> courseController.deleteData(req,res))
 
 module.exports = router;
